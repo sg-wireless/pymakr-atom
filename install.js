a7fed0915db2d16f119db0fd0d7d34e10748a135
Ralph Hogenbirk <ralph@pycom.io>
2017-04-13 18:09:24 +0200

    Improved sync stability and speed, added logger class for easier debugging

--- install.js
+++ install.js
@@ -1,8 +1,5 @@
-
-
 // installing and re-compiling serialport
-// not needed on windows, for which a pre-compiled version is included in the lib
-
+// not needed on windows, for which a pre-compiled version is included in the lib/connections/serialport-win folder
 if (process.platform != 'win32') {
   var exec = require('child_process').exec;
   exec('npm install serialport',
