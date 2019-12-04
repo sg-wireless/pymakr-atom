cc0fa38f94b1a0af0d81a089adb07d31e7df3d24
Pedro Knup <phknup@gmail.com>
2019-12-04 13:05:26 +0100

    feat: implemented new compatibility fix from pymakr 1.5

---
+++ native_modules/included_runtimes.md
@@ -0,0 +1,14 @@
+Includes support for electron/node versions:
+* electron 4.2.7 uses ABI 69
+   - win32   , x64  
+   - win32   , ia32 
+   - darwin  , x64  
+   - linux   , x64  
+   - linux   , ia32 
+Includes support for electron/node versions:
+* electron 4.2.7 uses ABI 69
+   - win32   , x64  
+   - win32   , ia32 
+   - darwin  , x64  
+   - linux   , x64  
+   - linux   , ia32 
---
+++ scripts/download-binaries.js
@@ -0,0 +1,66 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
+const gitToken = core.getInput("git-token") || process.env.GITHUB_TOKEN;
+const octokit = new github.GitHub(gitToken);
+const shell = require("node-powershell");
+
+const repo = {
+  owner: "atom",
+  repo: "atom"
+};
+
+let ps = new shell({
+  executionPolicy: "Bypass",
+  noProfile: true
+});
+const fetchElectronVersion = async tag => {
+  try {
+    // Fetch package.json file (contains electron target)
+    const response = await axios.get(
+      `https://raw.githubusercontent.com/atom/atom/${tag}/package.json`
+    );
+    const version = response.data.electronVersion.toString();
+    core.info(`Atom ${tag} uses Electron v${version}`);
+    return version;
+  } catch (e) {
+    throw e;
+  }
+};
+
+// gets binaries for current and nightly Atom version
+const getBinaries = async () => {
+  const tags = (
+    await octokit.repos.listTags({
+      ...repo,
+      per_page: 50
+    })
+  ).data.map(item => item.name);
+
+  const atomNightlyTag = tags[0];
+  const atomCurrentTag = "master";
+  const atomNightlyElectron = await fetchElectronVersion(atomNightlyTag);
+  const atomCurrentElectron = await fetchElectronVersion(atomCurrentTag);
+  const electronVersions = [];
+  electronVersions.push(atomCurrentElectron);
+  if (atomCurrentElectron != atomNightlyElectron)
+    electronVersions.push(atomNightlyElectron);
+  console.log("\n");
+  console.log(`Downloading binaries for ${electronVersions.join(", ")}`);
+  ps.addCommand(
+    "/Users/pk/dev/pycom/pymakr-atom/scripts/mp-download-atom.ps1",
+    [{ ElectronVersions: electronVersions }]
+  );
+
+  ps.invoke()
+    .then(output => {
+      console.log(output);
+      ps.dispose();
+    })
+    .catch(err => {
+      console.log(err);
+      ps.dispose();
+    });
+};
+
+getBinaries();
